be25508b76edc94e2ab10c47fde0de827a04193a
Keenan-Faure <keenan.asterisk@gmail.com>
2023-06-07 17:01:10 +0200

    update

--- internal/fetch/fetch.go
+++ internal/fetch/fetch.go
@@ -1,9 +1,57 @@
 package fetch
 
 import (
-	"fmt"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"strings"
 )
 
-func Fetch(url string, query_params map[string]string) {
-	fmt.Println("Hello Fetch!")
+type pokeloc struct {
+	Count    int    `json:"count"`
+	Next     string `json:"next"`
+	Previous any    `json:"previous"`
+	Results  []struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	} `json:"results"`
+}
+
+func GET(url string, query_params map[string]string) (pokeloc, error){
+	if(url != "") {
+		url = add_query_params(url, query_params)
+		resp, err := http.Get(url)
+		if(err != nil) {
+			return pokeloc{}, err
+		}
+		defer resp.Body.Close()
+		body, err := io.ReadAll(resp.Body)
+		if(err != nil) {
+			return pokeloc{},err
+		}
+		result := pokeloc{}
+		err_r := json.Unmarshal(body, &result)
+		if(err_r != nil) {
+			return pokeloc{}, err
+		}
+		return result, nil
+	}
+	return pokeloc{}, errors.New("undefined url")
+}
+
+func add_query_params(url string, query_params map[string]string) string {
+	iterator := 0
+	if(url != "" && query_params != nil) {
+		for key := range query_params {
+			if(iterator == 0) {
+				url = url + "?"
+				iterator ++
+			}
+			url = url + key + "=" + query_params[key] + "&"
+		}
+		url = strings.TrimSuffix(url, "&")
+		return url
+	}
+	return url
 }
--- main.go
+++ main.go
@@ -2,12 +2,23 @@ package main
 
 import (
 	"bufio"
+	"fetch"
 	"fmt"
-	"internal/fetch"
 	"os"
 )
 
-func commandHelp() error {
+type config struct {
+	offset string
+	limit  int
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func(conf *config) error
+}
+
+func commandHelp(conf config) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
 	fmt.Println("")
@@ -16,56 +27,58 @@ func commandHelp() error {
 	return nil
 }
 
-func commandExit() error {
+func commandExit(conf config) error {
 	os.Exit(0)
 	return nil
 }
 
-func commandMap() error {
-	fmt.Println("I am map")
-	//https://pokeapi.co/api/v2/location-area?offset=0&limit=10
+func commandMap(conf config) error {
+	resp, err := fetch.GET("https://pokeapi.co/api/v2/location-area/", query_params)
+	if(err != nil) {
+		fmt.Println(err.Error())
+	}
+	for value := range resp.Results {
+		fmt.Println(resp.Results[value].Name)
+	}
 	return nil
 }
-func commandMapb() error {
+func commandMapb(conf config) error {
 	fmt.Println("I am mapb")
 	return nil
 }
 
 func main() {
-	type cliCommand struct {
-		name        string
-		description string
-		callback    func() error
+	conf := config {
+		offset: "0",
+		limit: 20,
 	}
 	commands := map[string]cliCommand{
 		"help": {
 			name:        "help",
 			description: "Displays a help message",
-			callback:    commandHelp,
+			callback:    ,
 		},
 		"exit": {
 			name:        "exit",
 			description: "Exit the Pokedex",
-			callback:    commandExit,
+			callback:    commandExit(conf),
 		},
 		"map": {
 			name:        "map",
 			description: "Displays the names of 20 location areas",
-			callback:    commandMap,
+			callback:    commandMap(conf),
 		},
 		"mapb": {
 			name:        "mapb",
 			description: "Displays the previous 20 location areas",
-			callback:    commandMapb,
+			callback:    commandMapb(conf),
 		},
 	}
-
-	fetch.Fetch("abc123", make(map[string]string))
 	fmt.Print("Pokedex > ")
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
 		if value, ok := commands[scanner.Text()]; ok {
-			value.callback()
+			value.callback(&conf)
 		} else {
 			fmt.Println("Error: Unknown command '" + string(scanner.Text()) + "'")
 		}
